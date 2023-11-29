ea646031d1697fbe4590964e84f0823d56f9f7be
fontes-mrc <62485413+fontes-mrc@users.noreply.github.com>
2023-11-29 17:20:59 -0300

    style: auto format code

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,26 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export function handle({ event, resolve }) {
-	const get_lang = (event) => (event.route.id == '/br' ? 'pt-br' : 'en');
-	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace('%lang%', get_lang(event))
-	});
+    function get_attributes(event) {
+        const lang = event.route.id == "/br" ? "pt-br" : "en"
+        if (lang == "pt-br") {
+            return {
+                lang: "pt-br",
+                description:
+                    "Maurício Fontes, engenheiro de dados e desenvolvedor experiente, traz sua paixão por inovação e conhecimento para criar soluções inovadoras."
+            }
+        } else {
+            return {
+                lang: "en",
+                description:
+                    "Maurício Fontes, an experienced data engineer and developer, thrives on continuous learning, bringing expertise in data engineering and development to drive innovative solutions."
+            }
+        }
+    }
+
+    const attr = get_attributes(event)
+
+    return resolve(event, {
+        transformPageChunk: ({ html }) =>
+            html.replace("%lang%", attr.lang).replace("%description%", attr.description)
+    })
 }
--- src/lib/home/content/content.svelte
+++ src/lib/home/content/content.svelte
@@ -1,36 +1,36 @@
 <script>
-	// @ts-nocheck
-	export let modalProps = {};
+    // @ts-nocheck
+    export let modalProps = {}
 
-	export let corpus;
-	let { content } = corpus;
+    export let corpus
+    let { content } = corpus
 
-	import About from './about/about.svelte';
-	import Interests from './interests/interests.svelte';
-	import Projects from './projects/projects.svelte';
+    import About from "./about/about.svelte"
+    import Interests from "./interests/interests.svelte"
+    import Projects from "./projects/projects.svelte"
 </script>
 
 <div class="content">
-	<About corpus={content} />
-	<Interests corpus={content} />
-	<Projects corpus={content} bind:modalProps />
+    <About corpus={content} />
+    <Interests corpus={content} />
+    <Projects corpus={content} bind:modalProps />
 </div>
 
 <style>
-	.content {
-		margin: 2rem auto;
-		color: var(--light-gray);
-		padding: 3rem;
-		width: 90%;
-		max-width: 1200px;
-		backdrop-filter: blur(5px);
-		background-color: rgba(0, 0, 0, 0.05);
-		border-radius: 0.5rem;
-	}
+    .content {
+        margin: 2rem auto;
+        color: var(--light-gray);
+        padding: 3rem;
+        width: 90%;
+        max-width: 1200px;
+        backdrop-filter: blur(5px);
+        background-color: rgba(0, 0, 0, 0.05);
+        border-radius: 0.5rem;
+    }
 
-	@media (max-width: 768px) {
-		.content {
-			padding: 1rem;
-		}
-	}
+    @media (max-width: 768px) {
+        .content {
+            padding: 1rem;
+        }
+    }
 </style>
